5a9eddedc3b0cd2f08aa556c208a1d6b0f905178
aguerrero15 <student@Mine.local>
2013-12-11 10:40:10 -0600

    TeamFile and TeamList edit
    TeamFile and TeamList have been fixed and deemed usuable.  The only
    thing that is left to add to TeamFile is the other settings that need
    to be saved.

--- TeamFile.java
+++ TeamFile.java
@@ -6,17 +6,19 @@ public class TeamFile
     private String filePath;
     private String name;
     private String number;
-    
+
     public TeamFile(String path)
     {
         filePath = path;
-        load(path + ".xml");
+        number = path;
+        save("Team Number" , path);
+        save("Team Name", "New Team");
     }
-    
+
     Properties properties = new Properties();
     public void save(String key, String value) 
     {
-        String path = "teams/" + filePath + ".xml";
+        String path = System.getProperty("user.dir") + "/teams/" + filePath + ".xml";
         try {
             File file = new File(path);
             boolean exist = file.exists();
@@ -25,7 +27,7 @@ public class TeamFile
             }
             OutputStream write = new FileOutputStream(path);
             properties.setProperty(key, value);
-            properties.storeToXML(write, GUI.teamNumber.getText());
+            properties.storeToXML(write, filePath);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to save the team's file.");
@@ -34,9 +36,10 @@ public class TeamFile
 
     public void load(String path) 
     {
+
         try 
         {
-            InputStream read = new FileInputStream(path);
+            InputStream read = new FileInputStream(System.getProperty("user.dir") + "/teams/" + path + ".xml");
             properties.loadFromXML(read);
             String teamName = properties.getProperty("Team Name");
             String teamNumber = properties.getProperty("Team Number");
@@ -45,32 +48,32 @@ public class TeamFile
         } 
         catch (FileNotFoundException e) 
         {
-            System.out.println("File not Found");
+            System.out.println("File not Found: Load");
             save("Team Name", "New Team");
             save("Team Number", filePath);
             load(path);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Failure to read file.");
+            System.out.println("Failure to read file: Load");
         }
     }
 
     public void setTeamDetails(String name, String number) 
     {
-         this.name = name;
-         this.number = number;
+        this.name = name;
+        this.number = number;
     }
-    
+
     public String getName()
     {
         return name;
     }
-    
+
     public String getNumber()
     {
         return number;
     }
-    
+
     public void changeName(String name)
     {
         this.name = name;
--- TeamListWindow.java
+++ TeamListWindow.java
@@ -1,17 +1,81 @@
+import java.util.*;
+import java.io.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
 public class TeamListWindow extends JFrame implements ActionListener {
-    
-    TeamFile[] fileArray;
-    
-    public TeamListWindow(TeamFile[] teamFileArray) {
-        fileArray = new TeamFile[teamFileArray.length];
+    ArrayList<TeamFile> fileArray;
+
+    public TeamListWindow() 
+    {
+        fileArray = new ArrayList<TeamFile>();
+        loadFileToArray();
     }
-    
+
+    public void addTeam(String number)
+    {
+        fileArray.add(new TeamFile(number));
+    }
+
+    public ArrayList<TeamFile> getList()
+    {
+        return fileArray;
+    }
+
+    public void writeToFile(String info)
+    {
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + "/teams/teams.txt"));
+            System.out.println();
+            writer.write(info);
+            writer.newLine();
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("Failed to write to file");
+        }
+    }
+
+    public void saveArrayList()
+    {
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + "/teams/teams.txt"));
+            for(TeamFile file : fileArray)
+            {
+                writer.write(file.getNumber());
+                writer.newLine();
+            }
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("Failed to write to file: Save");
+        }
+    }
+
+    public void loadFileToArray()
+    {
+        try
+        {
+            Scanner reader = new Scanner(new File(System.getProperty("user.dir") + "/teams/teams.txt"));
+            while(reader.hasNext())
+            {
+                addTeam(reader.nextLine());
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+             System.out.println("Failed to read file: Read");
+        }
+    }
+
     public void actionPerformed(ActionEvent e) {
-        
     }
-    
 }
