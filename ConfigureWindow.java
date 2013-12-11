b8dfab6df4f40bd3d97bc42c14c35d244e5e69e4
aguerrero15 <student@Mine.local>
2013-12-11 12:27:06 -0600

    Again

--- ConfigureWindow.java
+++ ConfigureWindow.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+@SuppressWarnings("unchecked")
 public class ConfigureWindow extends JFrame implements ActionListener {
 
     protected JFrame window;
@@ -9,19 +10,21 @@ public class ConfigureWindow extends JFrame implements ActionListener {
 
     protected String[] irArray = new String[2];
 
+    
     public ConfigureWindow() {
         window = new JFrame("Configuration");
         window.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         window.getContentPane().setLayout(new BorderLayout());
         window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
         irArray[0] = "Yes";
         irArray[1] = "No";
-        irAuto = new JComboBox(irArray);
-        irAuto.setSelectedIndex(2);
-        irAuto.addActionListener(this);
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.add(irAuto);
+        //irAuto = new JComboBox(irArray);
+        //irAuto.setSelectedIndex(2);
+        //irAuto.addActionListener(this);
+        //JPanel buttonPanel = new JPanel(new FlowLayout());
+        //buttonPanel.add(irAuto);
 
         save = new JButton("Save");
         save.addActionListener(this);
@@ -32,7 +35,7 @@ public class ConfigureWindow extends JFrame implements ActionListener {
         titlePanel.add(new JLabel("Configure"));
 
         window.add(titlePanel, BorderLayout.NORTH);
-        window.add(buttonPanel, BorderLayout.CENTER);
+        //window.add(buttonPanel, BorderLayout.CENTER);
         window.add(savePanel, BorderLayout.SOUTH);
 
         window.pack();
